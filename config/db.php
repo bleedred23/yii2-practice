c7aec230897ec7a266bbe4764970ed79559d58b3
bleedred23 <shineisalliwant@gmail.com>
2021-10-03 20:47:20 +0300

    print env action

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=ec2-54-216-17-9.eu-west-1.compute.amazonaws.com::5432;dbname=dchi0ike84j4br',
+    'username' => 'sbhqdhgxwohzsi',
+    'password' => '665c23411d75fabf265a31275e813a60cd0a24b4d38bf9a8900f3a5c4448a492',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,4 +125,10 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+    public function actionEnv()
+    {
+        $url = parse_url(getenv("DATABASE_URL"));
+        var_dump($url);
+    }
 }
