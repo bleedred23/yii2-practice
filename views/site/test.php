3d905fb1df54e3be7fe432e18ef95eefb292b71f
bleedred23 <shineisalliwant@gmail.com>
2021-10-03 21:37:58 +0300

    new testpage

--- views/site/test.php
+++ views/site/test.php
@@ -6,10 +6,15 @@
 $this->title = 'Test page';
 
 try {
-    $connection = Yii::$app->db->createCommand('CREATE TABLE test (
+    $connection = Yii::$app->db;
+    $connection->createCommand('CREATE TABLE test (
         ID int,
         title text
     )')->queryAll();
+    print('$connection');
+    echo PHP_EOL;
+    var_dump($connection);
+    echo PHP_EOL;
 } catch (\yii\db\Exception $e) {
     print($e->getMessage());
 }
@@ -19,9 +24,4 @@ $url = parse_url(getenv("DATABASE_URL"));
 print('$url');
 echo PHP_EOL;
 var_dump($url);
-echo PHP_EOL;
-
-print('$connection');
-echo PHP_EOL;
-var_dump($connection);
 echo PHP_EOL;
